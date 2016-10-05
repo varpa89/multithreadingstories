58e4150f7468327f07cfdbde7401f2e369a4b322
Павел Варченко <p.varchenko@company.dnevnik.ru>
2016-10-05 14:33:27 +0300

    add abstraction

---
+++ src/main/java/jox/BrokenLeg.java
@@ -0,0 +1,24 @@
+package jox;
+
+import java.util.logging.Logger;
+
+/**
+ * @author p.varchenko
+ * @since 05.10.16
+ */
+public class BrokenLeg extends AbstractLeg {
+
+    BrokenLeg(String name, RouteData routeData) {
+        super(name, routeData);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (routeData.finish()) {
+                break;
+            }
+            checkAndMakeStep();
+        }
+    }
+}
